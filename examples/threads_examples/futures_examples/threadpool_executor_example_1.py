56d15720829785c096feea55c1710ef088199e95
danny <danny.gurman@gmail.com>
2019-06-09 22:44:43 +0300

    threads pool

---
+++ examples/threads_examples/futures_examples/threadpool_executor_example_1.py
@@ -0,0 +1,24 @@
+#https://tutorialedge.net/python/concurrency/python-threadpoolexecutor-tutorial/
+#ThreadPoolExecutors provide a simple abstraction around spinning up multiple
+# threads and using these threads to perform tasks in a concurrent fashion
+
+from concurrent.futures import ThreadPoolExecutor
+import threading
+# import random
+
+def task():
+    print("Executing our Task")
+    result = 0
+    i = 0
+    for i in range(10):
+        result = result + i
+    print("I: {}".format(result))
+    print("Task Executed {}".format(threading.current_thread()))
+
+def main():
+    executor = ThreadPoolExecutor(max_workers=3)
+    task1 = executor.submit(task)
+    task2 = executor.submit(task)
+
+if __name__ == '__main__':
+    main()

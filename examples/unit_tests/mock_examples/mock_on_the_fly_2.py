190e1706d5f3d235b66f6a35fcd7120e9bbca4ae
dannygurman <danny.gurman@gmail.com>
2019-06-10 16:46:26 +0300

    mock order

--- examples/unit_tests/mock_examples/mock_on_the_fly_2.py
+++ examples/unit_tests/mock_examples/mock_on_the_fly_2.py
@@ -4,14 +4,19 @@ from  unit_tests.mock_examples import fots
 import os
 
 
+
+#NOTE the order!!! (mocked_some_method parameter before  mocked_urandom) - Python calls @patch decorators from the "bottom up".
+
 class TestRandom(unittest.TestCase):
     @mock.patch('os.urandom')
     @mock.patch('unit_tests.mock_examples.fots.some_method')
-    def test_abc_urandom(self, mocked_urandom , mocked_some_method):
+    def test_abc_urandom(self , mocked_some_method ,mocked_urandom):
 
        mocked_urandom.return_value = 'pumpkins'
        assert os.urandom(5) == 'pumpkins'
-
+       mocked_some_method.return_value = "xxx"
+       result =  fots.some_method()
+       assert result == "xxx"
 
       # mocked_some_method.return_value = 'xxx'
       # assert fots.some_method() == 'xxx'

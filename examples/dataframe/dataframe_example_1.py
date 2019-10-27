e141de7a6172574e1cdaad5d36f1b1506b0d8ae5
dannygurman <danny.gurman@gmail.com>
2019-10-27 10:29:57 +0200

    dataframe

---
+++ examples/dataframe/dataframe_example_1.py
@@ -0,0 +1,21 @@
+# importing pandas as pd
+import pandas as pd
+
+# Creating the DataFrame
+df = pd.DataFrame({'Weight':[45, 88, 56, 15, 71],
+				'Name':['Sam', 'Andrea', 'Alex', 'Robin', 'Kia'],
+				'Age':[14, 25, 55, 8, 21]})
+
+# Print the DataFrame
+print(df)
+
+#    Weight    Name  Age
+# 0      45     Sam   14
+# 1      88  Andrea   25
+# 2      56    Alex   55
+# 3      15   Robin    8
+# 4      71     Kia   21
+
+values = df['Name']
+print(values[0])
+#Sam
